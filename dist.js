ee0195c406f72686c063c50a78d3eb0a1a89ec5b
Jasper De Moor <jasperdemoor@gmail.com>
2018-05-08 20:42:46 +0200

    fix publish pipeline

--- dist.js
+++ dist.js
@@ -1,8 +1,10 @@
 import stylish from './lib/stylish';
 import css from './lib/css/css';
+const styled = stylish;
 
 export default stylish;
 export {
   stylish,
+  styled,
   css
 };
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import stylish from './src/stylish';
 import css from './src/css/css';
-const styled = stylish;
 
 export default stylish;
 export {
   stylish,
-  styled,
   css
 };
