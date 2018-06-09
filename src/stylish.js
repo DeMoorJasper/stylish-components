0f1f546ed4107ac7f451f9a990313a959c1c1e15
Jasper De Moor <jasperdemoor@gmail.com>
2018-06-09 13:59:15 +0200

    small optimisation

--- src/stylish.js
+++ src/stylish.js
@@ -1,9 +1,7 @@
 import constructElement from './constructElement';
 import domElements from './utils/domElements';
 
-let stylish = {};
-domElements.forEach(el => {
-  stylish[el] = constructElement(el);
-});
-
-export default stylish;
+export default domElements.reduce((obj, el) => {
+  obj[el] = constructElement(el);
+  return obj;
+}, {});
