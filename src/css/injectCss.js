ba71be6eede10c9d0d175830ce5b09a6d8c72bef
Jasper De Moor <jasperdemoor@gmail.com>
2019-03-03 10:32:54 +0100

    SSR Fixes

--- src/css/injectCss.js
+++ src/css/injectCss.js
@@ -1,21 +1,27 @@
-import processCss from './processCss';
+import processCss from "./processCss";
 
 const getStyleContainer = () => {
-  let styleContainer = document.getElementById('stylish-components');
+  if (typeof document === 'undefined') return null;
+
+  let styleContainer = document.getElementById("stylish-components");
   if (!styleContainer) {
-    styleContainer = document.createElement('style');
-    styleContainer.id = 'stylish-components';
-    styleContainer.type = 'text/css';
+    styleContainer = document.createElement("style");
+    styleContainer.id = "stylish-components";
+    styleContainer.type = "text/css";
     document.head.appendChild(styleContainer);
   }
-  return styleContainer;
-}
 
-export default (css) => {
+  return styleContainer;
+};
+
+export default css => {
   let stylesheet = processCss(css);
   let styleContainer = getStyleContainer();
-  if (styleContainer.innerHTML.indexOf(stylesheet.hash) === -1) {
+  if (
+    styleContainer &&
+    styleContainer.innerHTML.indexOf(stylesheet.hash) === -1
+  ) {
     styleContainer.innerHTML += stylesheet.css;
   }
   return stylesheet.hash;
-}
+};
